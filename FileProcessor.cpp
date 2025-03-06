e0d4e16e15b9184f8604ec21218755892a9dce32
Marshall T Longtin <156374322+mtlongtin123@users.noreply.github.com>
2025-03-05 21:38:46 -0800

    Update FileProcessor.cpp

--- FileProcessor.cpp
+++ FileProcessor.cpp
@@ -12,10 +12,10 @@ using namespace std;
 
 
 FileProcessor::FileProcessor(string inFilename,string outFilename){
-    inFile.open(inFilename);
-    outFile.open(outFilename);
-    FileContents = new int[8]; 
-    ReadFile();
+    inFile.open(inFilename); // open input file
+    outFile.open(outFilename); // open output file
+    FileContents = new int[8]; // establish/assign FileContents
+    ReadFile(); // read the file output
     // intializing memory and allowing other files to access it
 } // need to know the constructor belongs to the file processor class
 
@@ -24,7 +24,7 @@ FileProcessor::~FileProcessor(){
     delete[] FileContents;  // Free allocated memory
     
     if (inFile.is_open()) inFile.close();  // Close files if open
-    if (outFile.is_open()) outFile.close();
+    if (outFile.is_open()) outFile.close(); 
 }
 
 
@@ -32,10 +32,10 @@ FileProcessor::~FileProcessor(){
 void FileProcessor::WriteToFile(string line){
     if (outFile.is_open()){
         std::cout << "Writing to file: " << line << std::endl;
-        std::cout << "File is open " << std::endl;
+        std::cout << "File is open " << std::endl; // Making sure file is open and then writing to it
         outFile << line << endl;
     } else {
-        std::cout << "File not open.";
+        std::cout << "File not open."; // If its not open print this
     }
     //outFile.close();
 }
@@ -44,10 +44,10 @@ void FileProcessor::WriteToFile(string line){
 
 
 int* FileProcessor::ReadFile(){
-    int i = 0;
+    int i = 0; 
     string line;
     while (inFile >> line){
-        FileContents[i] = stoi(line);
+        FileContents[i] = stoi(line); // Changing from string to an int
         ++i;
     }
     return FileContents; 
